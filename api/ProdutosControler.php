bbab683e4b7ccaa881cd430473d400f1f7ec3613
andreaqs2717 <141207602+andreaqs2717@users.noreply.github.com>
2023-08-02 03:15:27 -0300

    Update ProdutosControler.php

--- api/ProdutosControler.php
+++ api/ProdutosControler.php
@@ -1,23 +1,23 @@
 <?php;
 
-      try 
-      {  
-
-        $servidor = '74.63.213.142,4502';
-        $banco = 'MZKSBDComercialPontoHomeEsc';
-        $usuario = 'mzks42405093000184';
-        $senha = 'wZNS41W4';  
+      //try 
+      //{  
+        echo 'dea';
+        //$servidor = '74.63.213.142,4502';
+        //$banco = 'MZKSBDComercialPontoHomeEsc';
+        //$usuario = 'mzks42405093000184';
+        //$senha = 'wZNS41W4';  
       
-        $pdoConfig  = "sqlsrv:". "Server=" . $servidor . ";";
-        $pdoConfig .= "Database=".$banco.";";
-        $conn = new PDO($pdoConfig, $usuario, $senha);
-        echo 'conectar';
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //$pdoConfig  = "sqlsrv:". "Server=" . $servidor . ";";
+        //$pdoConfig .= "Database=".$banco.";";
+        //$conn = new PDO($pdoConfig, $usuario, $senha);
+        //echo 'conectar';
+        //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-     } 
-     catch (PDOException $e) 
-     {
-      echo "Erro na conexÃ£o: " . $e->getMessage();
-     }
+     //} 
+     //catch (PDOException $e) 
+     //{
+      //echo "Erro na conexÃ£o: " . $e->getMessage();
+     //}
    
 ?>
